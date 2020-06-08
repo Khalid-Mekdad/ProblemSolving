fb79f0b99c7244e8924edd689bd5b0b09f6fbd28
khalid_mekdad <khalid_abdelmonem@outlook.com>
2020-06-09 01:23:00 +0200

    Day 1 Easy

---
+++ ProblemSolving/ArraysProblems.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    static class ArraysProblems
+    {
+        //https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+
+            int greatestNmbOfCandies = -1;
+            for (int i = 0; i <= candies.Length - 1; i++)
+            {
+                greatestNmbOfCandies = Math.Max(greatestNmbOfCandies, candies[i]);
+            }
+
+            List<bool> result = new List<bool>(candies.Length);
+            for (int i = 0; i <= candies.Length - 1; i++)
+            {
+                result.Add(candies[i] + extraCandies >= greatestNmbOfCandies ? true : false);
+            }
+            return result;
+
+        }
+
+        //https://leetcode.com/problems/defanging-an-ip-address/
+        public static string DefangIPaddr(string address)
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i <= address.Length - 1; i += 1)
+            {
+                if (address[i] == '.')
+                {
+                    str.Append("[.]");
+                }
+                else
+                {
+                    str.Append(address[i]);
+                }
+            }
+            return str.ToString();
+        }
+
+        //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+        public static int NumberOfSteps(int num)
+        {
+            if (num == 0) return 0;
+            else
+            {
+                int stepsCount = 0;
+                while (num > 0)
+                {
+                    if (num % 2 == 0)
+                        num /= 2;
+                    else
+                        num -= 1;
+
+                    stepsCount++;
+                }
+
+                return stepsCount < 0 ? stepsCount - 1 : stepsCount;
+            }
+        }
+
+        //https://leetcode.com/problems/jewels-and-stones/
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            HashSet<int> types = new HashSet<int>();
+            for (int i = 0; i <= J.Length-1; i++)
+            {
+                types.Add(J[i]);
+            }
+            for (int i = 0; i <= S.Length - 1; i++)
+            {
+                if (types.Contains(S[i]))
+                    count++;
+                    
+            }
+            return count;
+        }
+    }
+}
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -6,7 +6,13 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var arr = new int[] { 2, 3, 5, 1, 3};
+
+            //var resu = ArraysProblems.KidsWithCandies(arr, 3);
+
+            Console.WriteLine(ArraysProblems.NumJewelsInStones("aA","aAAbbbb"));
+
         }
     }
 }
